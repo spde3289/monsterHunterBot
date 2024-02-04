1fef3e204d37e3f724500ff4c2dd8756ac32df1e
spde3289 <spde3289@naver.com>
2024-02-05 05:09:26 +0900

    소재 데이터 크롤링

---
+++ dateFatch.js
@@ -0,0 +1,53 @@
+const { Builder, Browser, By, until } = require("selenium-webdriver");
+const fs = require("fs");
+
+const selenium = async () => {
+  let driver = await new Builder().forBrowser(Browser.CHROME).build();
+  await driver.manage().setTimeouts({
+    implicit: 50000, // 10초
+    pageLoad: 30000, // 30초
+    script: 30000, // 30초
+  });
+  await driver.get(`https://mhworld.kiranico.com/ko/items`);
+
+  try {
+    const currentHtml = await driver.findElements(
+      By.className("btn btn-secondary")
+    );
+
+    currentHtml[1].click();
+
+    await driver.sleep(25000);
+
+    const a = await driver.findElements(By.className("col-lg-4 py-2"));
+
+    await setJson(a);
+  } finally {
+    await driver.quit();
+  }
+};
+
+const setJson = async (data) => {
+  const json = [];
+
+  for (let i = 0; i < data.length; i++) {
+    const text = await data[i].findElement(By.css("a")).getText();
+    const href = await data[i].findElement(By.css("a")).getAttribute("href");
+
+    json.push({
+      name: text,
+      href: href,
+    });
+  }
+
+  const stringJson = JSON.stringify(json);
+  fs.writeFileSync("product.json", stringJson);
+
+  return json;
+};
+
+selenium();
+
+module.exports = {
+  selenium: selenium,
+};
